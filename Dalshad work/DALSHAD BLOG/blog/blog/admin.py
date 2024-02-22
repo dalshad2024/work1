48c3c8c96500b4e0be9fa047d861d1e15725c61b
acram <85809502+acrambot@users.noreply.github.com>
2024-02-22 08:35:38 +0300

    Delete Dalshad work directory

--- Dalshad work/DALSHAD BLOG/blog/blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Author, Article
-
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ("name", "gender", "contact", "address")
-
-admin.site.register(Author,AuthorAdmin)
